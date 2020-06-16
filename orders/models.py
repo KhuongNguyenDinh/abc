2f143fe0381b0a9f8fce0ec992ff1db61efea202
nguyen khuong <dkhuong992810@gmail.com>
2020-06-16 22:33:47 +0700

    class comment and comment form in order/models.py

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from inside.models import SanPham
-
-
+from django.db import models
+from django.contrib.auth.models import User as UserAdmin
+from django.contrib.auth.models import User
+from django.forms import ModelForm
 class Order(models.Model):
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
@@ -34,4 +36,29 @@ class OrderItem(models.Model):
 
     def get_cost(self):
         return self.price * self.quantity
+
+
+class Comment(models.Model):
+    STATUS = (
+        ('New', 'New'),
+        ('True','True'),
+        ('False','False'),
+    )
+    product = models.ForeignKey(SanPham, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    subject = models.CharField(max_length=50, blank=True)
+    comment = models.CharField(max_length=250, blank=True)
+    rate = models.IntegerField(default = 1)
+    ip = models.CharField(max_length=20, blank = True)
+    status = models.CharField(max_length=10, choices=STATUS, default= 'New')
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['subject', 'comment', 'rate']
 
